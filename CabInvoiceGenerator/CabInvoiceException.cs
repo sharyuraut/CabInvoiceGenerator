fb541d76f1d2b7420de9cd05087527d3eac963b6
sharyu raut <sharyukraut210298@gmail.com>
2020-11-02 00:45:08 +0530

    [Sharyu] Add. Enhanced Invoice

--- CabInvoiceGenerator/CabInvoiceException.cs
+++ CabInvoiceGenerator/CabInvoiceException.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -22,4 +23,5 @@ namespace CabInvoiceGenerator
             this.type = type;
         }
     }
+
 }
