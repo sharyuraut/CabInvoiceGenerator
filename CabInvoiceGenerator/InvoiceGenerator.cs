91c768ff45d481fa78e8f3a7baf23fb2dea091d3
sharyu raut <sharyukraut210298@gmail.com>
2020-11-02 00:09:42 +0530

    [Sharyu] refactor. resolving issue of testlog

---
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceGenerator
+    {
+        //Variables
+        RideType rideType;
+        private RideRepository rideRepository;
+
+        //Constants
+        private readonly double MINIMUM_COST_PER_KM = 10;
+        private readonly int COST_PER_TIME = 1;
+        private readonly double MINIMUM_FARE = 5;
+
+        public InvoiceGenerator(RideType rideType)
+        {
+            this.rideType = rideType;
+            this.rideRepository = rideRepository;
+            try
+            {
+                if (rideType.Equals(RideType.PREMIUM))
+                {
+                    this.MINIMUM_COST_PER_KM = 10;
+                    this.COST_PER_TIME = 1;
+                    this.MINIMUM_FARE = 5;
+                }
+            }
+            catch (CabInvoiceException)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_RIDE_TYPE, "Invalid Ride Type");
+            }
+        }
+        public double CalculateFare(double distance, int time)
+        {
+            double totalFare = 0;
+            try
+            {
+                totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+            }
+            catch (CabInvoiceException)
+            {
+                if (rideType.Equals(null))
+                {
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_RIDE_TYPE, "Invalid Ride Type");
+                }
+                if (distance <= 0)
+                {
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_DISTANCE, "Invalid Distance");
+                }
+                if (time < 0)
+                {
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_TIME, "Invalid Time");
+                }
+            }
+            return Math.Max(totalFare, MINIMUM_FARE);
+        }
+    }
+}
