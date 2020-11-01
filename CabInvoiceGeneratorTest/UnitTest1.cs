649fef82d7bfde2a2428f904dc64d77700d6ec2a
sharyu raut <sharyukraut210298@gmail.com>
2020-11-01 20:54:23 +0530

    [Sharyu] Add. uc1 calculating basic fare

---
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CabInvoiceGeneratorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
